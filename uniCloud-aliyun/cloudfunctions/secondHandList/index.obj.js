02ce9dadf77cd344cb3a2e51896d7b066e44d6c4
ConstFiv <710144136@qq.com>
2022-08-29 17:59:19 +0800

    feat : 现在二手市场的瀑布流数据都是从后端动态获取到的

--- uniCloud-aliyun/cloudfunctions/secondHandList/index.obj.js
+++ uniCloud-aliyun/cloudfunctions/secondHandList/index.obj.js
@@ -1,11 +1,14 @@
 // 云对象教程: https://uniapp.dcloud.net.cn/uniCloud/cloud-obj
 // jsdoc语法提示教程：https://ask.dcloud.net.cn/docs/#//ask.dcloud.net.cn/article/129
 module.exports = {
-	// 获取二手列表信息
-	getCardsInfo(){
-		return{
-			errCode:0,
-			data:"我是getCardsInfo啊啊啊啊啊",
-		}
+	// 获取全部二手列表信息
+	async getCardsInfo(){
+		const db = uniCloud.database()
+		const cardsInfoData = db.collection('fiv-sale-list')
+		let  res = await cardsInfoData.get()
+		res.errCode = 0
+		console.log("获取全部二手列表信息",res)
+		return res
+		
 	}
 }
