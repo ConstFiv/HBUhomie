12344bde0ea82762962a59c34bdb93abce7ba8b8
ConstFiv <710144136@qq.com>
2022-08-29 16:36:32 +0800

    feat : 完成uniCloud搭建后台，并对前台请求进行响应

---
+++ uniCloud-aliyun/cloudfunctions/secondHandList/index.obj.js
@@ -0,0 +1,11 @@
+// 云对象教程: https://uniapp.dcloud.net.cn/uniCloud/cloud-obj
+// jsdoc语法提示教程：https://ask.dcloud.net.cn/docs/#//ask.dcloud.net.cn/article/129
+module.exports = {
+	// 获取二手列表信息
+	getCardsInfo(){
+		return{
+			errCode:0,
+			data:"我是getCardsInfo啊啊啊啊啊",
+		}
+	}
+}
