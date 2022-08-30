90968f5f7122274a63fe9789fc1a726b5ef5cfe6
ConstFiv <710144136@qq.com>
2022-08-30 11:39:52 +0800

    feat : 现在可以自动识别下滑做分页查询

--- uniCloud-aliyun/cloudfunctions/secondHandList/index.obj.js
+++ uniCloud-aliyun/cloudfunctions/secondHandList/index.obj.js
@@ -1,14 +1,27 @@
-// 云对象教程: https://uniapp.dcloud.net.cn/uniCloud/cloud-obj
-// jsdoc语法提示教程：https://ask.dcloud.net.cn/docs/#//ask.dcloud.net.cn/article/129
+
 module.exports = {
-	// 获取全部二手列表信息
-	async getCardsInfo(){
+	
+	// 获取全部二手列表信息  @requestTime请求次数
+	async getCardsInfo(requestTime){
+		console.log("获取全部二手列表信息  @requestTime请求次数",requestTime)
+		if(requestTime==undefined || requestTime==null){
+			return {
+				errCode:-1,
+				message:"缺少参数requestTime!",
+				data:[],
+			}
+		}
+		
 		const db = uniCloud.database()
-		const cardsInfoData = db.collection('fiv-sale-list')
+		const cardsInfoData = db.collection('fiv-sale-list').skip(5*requestTime).limit(5)
 		let  res = await cardsInfoData.get()
 		if(res){
 			res.errCode = 0
+			// 每次取表中的指定项
+			// if(res.data.length>=requestTime*5) res.data = res.data[requestTime-1,requestTime*5-1]
+			// else  res.data = res.data[requestTime-1,res.data.length-1]
 			return res
+		
 		}else{
 			return {
 				errCode:-1,
