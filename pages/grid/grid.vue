d405cb6db14626578c962a42aa9d8c7ba43365ca
ConstFiv <710144136@qq.com>
2022-08-30 14:58:09 +0800

    feat : 新建一个post组件，用于发布信息页面的承载

--- pages/grid/grid.vue
+++ pages/grid/grid.vue
@@ -52,6 +52,7 @@
 		},
 		data() {
 			return {
+				// 宫格信息
 				gridList: [
 					{src:'https://img2.baidu.com/it/u=3895119537,2684520677&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500',
 					title:"发布闲置"},
@@ -84,10 +85,11 @@
 		},
 		methods: {
 			change(e) {
-				uni.showToast({
-					title:this.$t('grid.clickTip') + " " + `${e.detail.index}` + " " + this.$t('grid.clickTipGrid'),
-					icon: 'none'
-				})
+				// uni.showToast({
+				// 	title:this.$t('grid.clickTip') + " " + `${e.detail.index}` + " " + this.$t('grid.clickTipGrid'),
+				// 	icon: 'none'
+				// })
+				this.$router.push('/pages/list/detail?id='+item._id+'&title='+item.title)
 			},
 			/**
 			 * banner加载后触发的回调
